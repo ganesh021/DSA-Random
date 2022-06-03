a8ac93234abbc3f73fbc23bda822ad05eb5374a9
Ganesh Patil <73637980+ganesh021@users.noreply.github.com>
2022-06-03 11:59:59 +0530

    Update Delete Middle Element of a Stack.py

--- Delete Middle Element of a Stack.py
+++ Delete Middle Element of a Stack.py
@@ -7,6 +7,9 @@ def deleteMiddleElement (stack, k):
     deleteMiddleElement(stack, k-1)
     stack.append(temp)              # append remaining elements one by one
 
+    
+    
+    
 #driver code
 stack = [2,3,4,5,6]
 k = len(stack)//2 + 1   # given in question - where Kth element from last needs to be removed
